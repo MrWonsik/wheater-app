1b8ca7ad4f756c8e9aae2fa3ae0368f99cba3eb9
MrWonsik <mrt.wonsik@gmail.com>
2024-05-06 21:52:52 +0200

    update deploy script

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-          Tomek Wąsacz test!
+          Tomek Wąsacz test #2
       </p>
     </>
   )
